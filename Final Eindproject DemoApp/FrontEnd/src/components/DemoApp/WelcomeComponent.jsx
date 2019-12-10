1b1b97e9732d8b4b28c671eb405205a47746baaa
hcsin <54602584+hcsin@users.noreply.github.com>
2019-12-10 12:04:45 +0100

    Update WelcomeComponent.jsx

--- Final Eindproject DemoApp/FrontEnd/src/components/DemoApp/WelcomeComponent.jsx
+++ Final Eindproject DemoApp/FrontEnd/src/components/DemoApp/WelcomeComponent.jsx
@@ -8,7 +8,7 @@ class WelcomeComponent extends Component {
     render(){     
         return <div>
             <h1 className="title">Welcome {username}!</h1>
-<!--        <h1 className="title">Welcome {this.props.match.params.name}!</h1> -->
+ {/* <h1 className="title">Welcome {this.props.match.params.name}!</h1>  */}
             <div className="container">
                 {/* {AuthenticationService.isUserAdmin() && <div>You are logged in as Admin</div>} */}
             </div>
