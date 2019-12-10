4cdff02eb226a64a9f7ecc41391f2c85731b6906
hcsin <54602584+hcsin@users.noreply.github.com>
2019-12-10 12:04:14 +0100

    Update WelcomeComponent.jsx
    naam ophalen van AuthenticationService

--- Final Eindproject DemoApp/FrontEnd/src/components/DemoApp/WelcomeComponent.jsx
+++ Final Eindproject DemoApp/FrontEnd/src/components/DemoApp/WelcomeComponent.jsx
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 import AuthenticationService from './AuthenticationService.js'
+const username = AuthenticationService.getLoggedInUsername();
 
 
 class WelcomeComponent extends Component {
 
     render(){     
         return <div>
-            <h1 className="title">Welcome {this.props.match.params.name}!</h1>
+            <h1 className="title">Welcome {username}!</h1>
+<!--        <h1 className="title">Welcome {this.props.match.params.name}!</h1> -->
             <div className="container">
                 {/* {AuthenticationService.isUserAdmin() && <div>You are logged in as Admin</div>} */}
             </div>
