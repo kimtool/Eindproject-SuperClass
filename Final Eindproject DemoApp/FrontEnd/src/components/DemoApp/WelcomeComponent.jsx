2a0a9c4434b3c876f6d2cc31206a44413d0220ed
hcsin <54602584+hcsin@users.noreply.github.com>
2019-12-16 23:20:12 +0100

    Update WelcomeComponent.jsx
    naam uit session storage

--- Final Eindproject DemoApp/FrontEnd/src/components/DemoApp/WelcomeComponent.jsx
+++ Final Eindproject DemoApp/FrontEnd/src/components/DemoApp/WelcomeComponent.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
-import AuthenticationService from "./AuthenticationService";
+import AuthenticationService, {USER_NAME_SESSION_ATTRIBUTE_NAME} from "./AuthenticationService";
 import Welcome_Image from '../images/afbeeldingen/welcomeBanner7.jpg'
-const username = AuthenticationService.getLoggedInUsername();
 
 class WelcomeComponent extends Component {
 
-    render(){     
+    render(){
+        const username = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME)
+        // console.log(username)
         return <div>
             <h1 className="title" style={{letterSpacing: "5px"}}>Welcome {username}!</h1>
             <img id="welcome-image" alt="" src={Welcome_Image}/>
