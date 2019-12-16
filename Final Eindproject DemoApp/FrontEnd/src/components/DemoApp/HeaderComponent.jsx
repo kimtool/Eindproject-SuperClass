41417d7c8dde80e1d97f5f7e151e611a1205b023
hcsin <54602584+hcsin@users.noreply.github.com>
2019-12-16 23:45:48 +0100

    Update HeaderComponent.jsx

--- Final Eindproject DemoApp/FrontEnd/src/components/DemoApp/HeaderComponent.jsx
+++ Final Eindproject DemoApp/FrontEnd/src/components/DemoApp/HeaderComponent.jsx
@@ -3,14 +3,47 @@ import AuthenticationService from './AuthenticationService.js'
 import {Link, withRouter} from 'react-router-dom'
 import hexagonlogo from '../images/hexagonlogo_A1_Rectangle_13_pattern.png'
 import '../../Header.css'
+import {USER_NAME_SESSION_ATTRIBUTE_NAME} from "./AuthenticationService";
+import {API_URL} from "../../Constants";
 
 class HeaderComponent extends Component{
+    emptyItem = {
+        id: '',
+        name: '',
+        email: '',
+        password: '',
+        role: ''
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            item: this.emptyItem
+        };
+    }
+
+    async componentDidMount() {
+        const username = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME)
+        if (username !== 'new') {
+            const user = await (await fetch(`${API_URL}/members/${username}`)).json();
+            this.setState({item: user});
+        }
+    }
+
+
     render(){
         const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
+        const isAdmin = this.state.role != 'ROLE_ADMIN';
         return (
-            <header className="desktop-header">                                     
+            <header className="desktop-header">
                 <div className="icon-block">{isUserLoggedIn && <Link className="menu-icon" to="/welcome">HOME</Link>}</div>
-                <div className="icon-block left">{isUserLoggedIn && <Link className="menu-icon" to="/profile">MY PROFILE</Link>}</div>
+                <div className="icon-block left">
+                {isAdmin ? (
+                    <div className="icon-block left">{isUserLoggedIn && <Link className="menu-icon" to="/profile">MY PROFILE</Link>}</div>
+                ) : (
+                    <div className="icon-block left">{isUserLoggedIn && <Link className="menu-icon" to="/profile">MEMBERS</Link>}</div>
+                )}
+                </div>
                 <Link to="/welcome"><img className="hexagonlogo" alt="" src={hexagonlogo}/></Link>
                 <div className="icon-block right">{isUserLoggedIn && <Link className="menu-icon"  to="/demos">DEMO'S</Link>}</div>
                 <div className="icon-block">{isUserLoggedIn && <Link className="menu-icon" to="/logout" onClick={AuthenticationService.logout}>LOG OUT</Link>}</div>
