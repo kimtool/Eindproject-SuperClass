788f475c4a8fac1139a76818513c51330e062e94
hcsin <54602584+hcsin@users.noreply.github.com>
2019-12-18 11:24:39 +0100

    Update SQL.sql

--- Final Eindproject DemoApp/BackEnd DemoApp/src/main/resources/SQL.sql
+++ Final Eindproject DemoApp/BackEnd DemoApp/src/main/resources/SQL.sql
@@ -10,6 +10,8 @@
 
 INSERT INTO `demodrop_database`.`user` (`id`, `email`, `password`, `role`, `username`) 
 VALUES ('1', 'admin01@dd.nl', '$2y$12$qYSszhFjZYpdxxxOTx95Q.qcTzSKVlL7zC5NuRT5QuMkvXR6F1/Oa', 'ROLE_ADMIN', 'admin');
+/*password = admin*/
 
 INSERT INTO `demodrop_database`.`user` (`id`, `email`, `password`, `role`, `username`) 
 VALUES ('2', 'user01@dd.nl', '$2y$12$Mu9E0gDEjiftcr9v2BJdBeUnYaMRVqPj9TiI2xu64W4ZnSAJSGZ7K', 'ROLE_USER', 'user');
+/*password = user*/
